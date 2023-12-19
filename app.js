a25ab88c9294afc5e4520add1856ff2a95d8b82c
Charlie Alders <charliealders26@gmail.com>
2023-12-19 00:16:46 -0800

    added 370k english words and a randomWord endpoint

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const { readFileSync } = require('fs');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.get('/api/words', (req, res) => {
+const englishWords = readFileSync('./data/english.json');
+
+app.get('/api/random_word', (req, res) => {
     const language = req.query["language"];
-    const through = req.query["through"];
-    res.json("Hit the api!");
+    let randomWord = '';
+    if (language == "english") {
+        randomWord = englishWords.keys()[Math.floor(Math.random() * 370101)];
+    }
+    res.json(randomWord);
 });
 
 app.listen(PORT, () => {
